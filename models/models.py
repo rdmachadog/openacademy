15aa4d9349f024c36c9681df27bfaa8b2ed8482c
rdmachadog <ruben@vauxoo.com>
2017-03-04 04:11:42 +0000

    [IMP] added relationship between sessions and courses

--- models/models.py
+++ models/models.py
@@ -9,11 +9,13 @@ class Course(models.Model):
 #     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
     responsible = fields.Many2one('res.users')
+    sessions = fields.One2many('session','course')
 
 class Session(models.Model):
     _name = 'session'
     name = fields.Char()
     instructor = fields.Many2one('res.partner')
+    course = fields.Many2one('course')
     start_date = fields.Date()
     duration = fields.Float(help="Duration in days")
     seats = fields.Integer()
