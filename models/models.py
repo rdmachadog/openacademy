41e22ce6da48b05a009dd81761c99cae90f82a68
rdmachadog <ruben@vauxoo.com>
2017-03-04 03:25:42 +0000

    [IMP] added sessions model with action,menu and views

--- models/models.py
+++ models/models.py
@@ -4,11 +4,20 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'course'
-
     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
+
+
+class Session(models.Model):
+    _name = 'session'
+    name = fields.Char()
+    start_date = fields.Date()
+    duration = fields.Float(help="Duration in days")
+    seats = fields.Integer()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
 #
 #     @api.depends('value')
 #     def _value_pc(self):
