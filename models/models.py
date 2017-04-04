037215f2c82d72ff5df104552061f7492d0a7129
rdmachadog <ruben@vauxoo.com>
2017-04-04 22:11:34 +0000

    Added workflow for courses records

--- models/models.py
+++ models/models.py
@@ -10,11 +10,28 @@ class Course(models.Model):
     description = fields.Text()
     responsible = fields.Many2one('res.users')
     sessions = fields.One2many('session','course')
+    state = fields.Selection([
+                              ('draft','Draft'),
+                              ('confirmed','Confirmed'),
+                              ('done','Done'),
+                             ], default='draft', readonly=True)
+    @api.one
+    def action_draft(self):
+        self.state = 'draft'
+
+    @api.one
+    def action_confirm(self):
+        self.state = 'confirmed'
+
+    @api.one
+    def action_done(self):
+        self.state = 'done'
+
 
 class Session(models.Model):
     _name = 'session'
     name = fields.Char()
-    instructor = fields.Many2one('res.partner')
+    instructor = fields.Many2one('res.partner', domain=[("instructor","=",True)])
     course = fields.Many2one('course')
     start_date = fields.Date()
     duration = fields.Float(help="Duration in days")
@@ -22,7 +39,7 @@ class Session(models.Model):
     attendees = fields.Many2many('res.partner')
 #     value = fields.Integer()
     percentage_seats_taken = fields.Float(compute="_compute_percentage_seats_taken", store='True')
-#
+
     @api.depends('attendees','seats')
     def _compute_percentage_seats_taken(self):
         for record in self:
@@ -31,3 +48,19 @@ class Session(models.Model):
             else:
                 record.percentage_seats_taken = 0.00
 
+    @api.onchange('seats','attendees')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendees):
+            return {
+                'warning': {
+                    'title': "Too mane attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
